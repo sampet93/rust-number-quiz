5f85e440cf97e1616b8cdd3c5a70983a9b1cef39
sampet93 <samppa1_93@hotmail.com>
2023-11-28 22:49:54 +0200

    Add input validation, refactor input reading

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,14 @@ const MAX_NUMBER: i32 = 10;
 
 fn main() {
     let mut guess_count = 0;
-    let mut guess;
     let mut rng = thread_rng();
     let correct_number = rng.gen_range(MIN_NUMBER..MAX_NUMBER);
 
     println!("Welcome to an amazing guessing game!");
     println!("Pick a number between {MIN_NUMBER} and {MAX_NUMBER}");
 
-    guess = read_input();
+    let mut guess_number = read_number_input();
 
-    let mut guess_number = covert_to_int(guess);
     guess_count += 1;
 
     while guess_number != correct_number {
@@ -24,27 +22,28 @@ fn main() {
 
         println!("Incorrect! Guess again: ");
 
-        guess = read_input();
-        guess_number = covert_to_int(guess);
+        guess_number = read_number_input();
     }
 
     println!("Correct! You tried {guess_count} time(s)");
 }
 
-fn read_input() -> String {
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read input");
+fn read_number_input() -> i32 {
+    loop {
+        let mut input = String::new();
 
-    return input;
-}
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
 
-fn covert_to_int(input: String) -> i32 {
-    input
-        .trim_start()
-        .trim_end()
-        .to_string()
-        .parse::<i32>()
-        .unwrap()
+        let input: i32 = match input.trim().parse::<i32>() {
+            Ok(parsed_input) => parsed_input,
+            Err(_) => {
+                println!("Please enter a valid number");
+                continue;
+            }
+        };
+
+        return input;
+    }
 }
